4a37ea65f64749225982d1f936f34a5cb24425a8
Dylan Yarbrough <129237422+DillDogG@users.noreply.github.com>
2025-06-01 17:57:15 -0600

    Facade Method start

--- StateSystem.cs
+++ StateSystem.cs
@@ -117,4 +117,85 @@ namespace MyFirstWinform
             return state.returnCurrentImage();
         }
     }
+
+
+
+    class StateKeeperImplementation
+    {
+        sectionGuessContext[] lastGuessesState1 = new sectionGuessContext[2];
+        sectionGuessContext[] lastGuessesState2 = new sectionGuessContext[2];
+        sectionGuessContext[] lastGuessesState3 = new sectionGuessContext[2];
+
+        NoState noState = new NoState();
+        IncorrectState incorrectState = new IncorrectState();
+        CloseState closeState = new CloseState();
+        CorrectState correctState = new CorrectState();
+        WinState winState = new WinState();
+        OutOfGuessesState outOfGuessesState = new OutOfGuessesState();
+
+        public void SetLastGuessesState(int section, bool correct, bool sectionOver)
+        {
+            if (correct)
+            {
+                if (section == 1)
+                {
+                    lastGuessesState1[1].setState(lastGuessesState1[0].state);
+                    lastGuessesState1[0].setState(getCorrectOptions(sectionOver));
+                }
+                else if (section == 2)
+                {
+                    lastGuessesState2[1].setState(lastGuessesState2[0].state);
+                    lastGuessesState2[0].setState(getCorrectOptions(sectionOver));
+                }
+                else if (section == 3)
+                {
+                    lastGuessesState3[1].setState(lastGuessesState3[0].state);
+                    lastGuessesState3[0].setState(getCorrectOptions(sectionOver));
+                }
+            }
+            else
+            {
+                if (section == 1)
+                {
+                    lastGuessesState1[1].setState(lastGuessesState1[0].state);
+                    lastGuessesState1[0].setState(getIncorrectOptions(sectionOver));
+                }
+                else if (section == 2)
+                {
+                    lastGuessesState2[1].setState(lastGuessesState2[0].state);
+                    lastGuessesState2[0].setState(getIncorrectOptions(sectionOver));
+                }
+                else if (section == 3)
+                {
+                    lastGuessesState3[1].setState(lastGuessesState3[0].state);
+                    lastGuessesState3[0].setState(getIncorrectOptions(sectionOver));
+                }
+            }
+        }
+
+        public sectionGuessState getIncorrectOptions(bool sectionOver)
+        {
+            if (sectionOver)
+            {
+                return outOfGuessesState;
+            }
+            else
+            {
+                return incorrectState;
+            }
+        }
+
+        public sectionGuessState getCorrectOptions(bool sectionOver)
+        {
+            if (sectionOver)
+            {
+                return winState;
+            }
+            else
+            {
+                return correctState;
+            }
+        }
+
+    }
 }
