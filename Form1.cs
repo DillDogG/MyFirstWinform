97d1a9530beee69f67236fd54faae6814b553ae6
Dylan Yarbrough <129237422+DillDogG@users.noreply.github.com>
2025-06-01 18:31:13 -0600

    Facade System Finished

--- Form1.cs
+++ Form1.cs
@@ -19,20 +19,11 @@ public partial class Form1 : Form
     private int guessCount3;
     //private 
     private int[] lastGuesses1 = new int[2];
-    private sectionGuessContext[] lastGuessesState1 = new sectionGuessContext[2];
     private int[] lastGuesses2 = new int[2];
-    private sectionGuessContext[] lastGuessesState2 = new sectionGuessContext[2];
     private int[] lastGuesses3 = new int[2];
-    private sectionGuessContext[] lastGuessesState3 = new sectionGuessContext[2];
     private const int guessMax = 5;
+    private StateKeeperImplementation stateKeeper = new StateKeeperImplementation();
 
-    // making all the states to save on memory
-    private NoState noState = new NoState();
-    private IncorrectState incorrectState = new IncorrectState();
-    private CloseState closeState = new CloseState();
-    private CorrectState correctState = new CorrectState();
-    private WinState winState = new WinState();
-    private OutOfGuessesState outOfGuessesState = new OutOfGuessesState();
     public Form1()
     {
         InitializeComponent();
@@ -49,15 +40,6 @@ public partial class Form1 : Form
         guessDisplay1.Text = "Guesses Left: " + guessMax;
         guessDisplay2.Text = "Guesses Left: " + guessMax;
         guessDisplay3.Text = "Guesses Left: " + guessMax;
-        lastGuessesState1[0] = new sectionGuessContext();
-        lastGuessesState1[1] = new sectionGuessContext();
-        lastGuessesState1[0].setState(noState);
-        lastGuessesState2[0] = new sectionGuessContext();
-        lastGuessesState2[1] = new sectionGuessContext();
-        lastGuessesState2[0].setState(noState);
-        lastGuessesState3[0] = new sectionGuessContext();
-        lastGuessesState3[1] = new sectionGuessContext();
-        lastGuessesState3[0].setState(noState);
         submit1.Enabled = true;
         submit2.Enabled = true;
         submit3.Enabled = true;
@@ -74,20 +56,19 @@ public partial class Form1 : Form
         guessDisplay1.Text = "Guesses Left: " + (guessMax - guessCount1);
 
         lastGuesses1[1] = lastGuesses1[0];
-        lastGuessesState1[1].setState(check_guess(lastGuesses1[1], numberOne));
 
         lastGuesses1[0] = (int)numericUpDown1.Value;
-        lastGuessesState1[0].setState(check_guess(numericUpDown1.Value, numberOne));
+        check_guess(lastGuesses1[0], numberOne, 1);
 
-        History1.Text = lastGuesses1[0] + " was " + lastGuessesState1[0].request() + "\r\n" + lastGuesses1[1] + " was " + lastGuessesState1[1].request();
+        History1.Text = lastGuesses1[0] + " was " + stateKeeper.GetState(1) + "\r\n" + lastGuesses1[1] + " was " + stateKeeper.GetState(1, false);
 
         if (guessCount1 >= guessMax)
         {
-            lastGuessesState1[0].setState(outOfGuessesState);
+            stateKeeper.SetLastGuessesState(false, true, 1);
             submit1.Enabled = false;
         }
         check_win();
-        result1.Image = lastGuessesState1[0].getImage();
+        result1.Image = stateKeeper.GetImage(1);
     }
 
     private void submit2_Click(object sender, EventArgs e)
@@ -96,20 +77,19 @@ public partial class Form1 : Form
         guessDisplay2.Text = "Guesses Left: " + (guessMax - guessCount2);
 
         lastGuesses2[1] = lastGuesses2[0];
-        lastGuessesState2[1].setState(check_guess(lastGuesses2[1], numberTwo));
 
         lastGuesses2[0] = (int)numericUpDown2.Value;
-        lastGuessesState2[0].setState(check_guess(numericUpDown2.Value, numberTwo));
+        check_guess(lastGuesses2[0], numberTwo, 2);
 
-        History2.Text = lastGuesses2[0] + " was " + lastGuessesState2[0].request() + "\r\n" + lastGuesses2[1] + " was " + lastGuessesState2[1].request();
+        History2.Text = lastGuesses2[0] + " was " + stateKeeper.GetState(2) + "\r\n" + lastGuesses2[1] + " was " + stateKeeper.GetState(2, false);
 
         if (guessCount2 >= guessMax)
         {
-            lastGuessesState2[0].setState(outOfGuessesState);
+            stateKeeper.SetLastGuessesState(false, true, 2);
             submit2.Enabled = false;
         }
         check_win();
-        result2.Image = lastGuessesState2[0].getImage();
+        result2.Image = stateKeeper.GetImage(2);
     }
 
     private void submit3_Click(object sender, EventArgs e)
@@ -118,20 +98,19 @@ public partial class Form1 : Form
         guessDisplay3.Text = "Guesses Left: " + (guessMax - guessCount3);
 
         lastGuesses3[1] = lastGuesses3[0];
-        lastGuessesState3[1].setState(check_guess(lastGuesses3[1], numberThree));
 
         lastGuesses3[0] = (int)numericUpDown3.Value;
-        lastGuessesState3[0].setState(check_guess(numericUpDown3.Value, numberThree));
+        check_guess(lastGuesses3[0], numberThree, 3);
 
-        History3.Text = lastGuesses3[0] + " was " + lastGuessesState3[0].request() + "\r\n" + lastGuesses3[1] + " was " + lastGuessesState3[1].request();
+        History3.Text = lastGuesses3[0] + " was " + stateKeeper.GetState(3) + "\r\n" + lastGuesses3[1] + " was " + stateKeeper.GetState(3, false);
 
         if (guessCount3 >= guessMax)
         {
-            lastGuessesState3[0].setState(outOfGuessesState);
+            stateKeeper.SetLastGuessesState(false, true, 3);
             submit3.Enabled = false;
         }
         check_win();
-        result3.Image = lastGuessesState3[0].getImage();
+        result3.Image = stateKeeper.GetImage(3);
     }
 
     private void numericUpDown1_ValueChanged(object sender, EventArgs e)
@@ -166,41 +145,34 @@ public partial class Form1 : Form
         answer.Text = "";
         lastGuesses1[0] = 0;
         lastGuesses1[1] = 0;
-        lastGuessesState1[0].setState(noState);
-        lastGuessesState1[1].setState(noState);
         lastGuesses2[0] = 0;
         lastGuesses2[1] = 0;
-        lastGuessesState2[0].setState(noState);
-        lastGuessesState2[1].setState(noState);
         lastGuesses3[0] = 0;
         lastGuesses3[1] = 0;
-        lastGuessesState3[0].setState(noState);
-        lastGuessesState3[1].setState(noState);
+        stateKeeper.ResetKeeper();
         submit1.Enabled = true;
         submit2.Enabled = true;
         submit3.Enabled = true;
     }
 
-    private sectionGuessState check_guess(decimal check, int answer)
+    private void check_guess(decimal check, int answer, int section)
     {
-        if (check == answer) { return correctState; }
-        if (check + 1 == answer || check - 1 == answer) { return closeState; }
-        return incorrectState;
+        if (check == answer) stateKeeper.SetLastGuessesState(true, false, section);
+        else if (check + 1 == answer || check - 1 == answer) stateKeeper.SetLastGuessesState(false, false, section, true);
+        else stateKeeper.SetLastGuessesState(false, false, section);
     }
 
     private void check_win()
     {
         if (lastGuesses1[0] == numberOne && lastGuesses2[0] == numberTwo && lastGuesses3[0] == numberThree)
         {
-            lastGuessesState1[0].setState(winState);
-            lastGuessesState2[0].setState(winState);
-            lastGuessesState3[0].setState(winState);
+            stateKeeper.SetLastGuessesState(true, true);
             submit1.Enabled = false;
             submit2.Enabled = false;
             submit3.Enabled = false;
-            result1.Image = lastGuessesState1[0].getImage();
-            result2.Image = lastGuessesState2[0].getImage();
-            result3.Image = lastGuessesState3[0].getImage();
+            result1.Image = stateKeeper.GetImage(1);
+            result2.Image = stateKeeper.GetImage(2);
+            result3.Image = stateKeeper.GetImage(3);
         }
     }
 
--- StateSystem.cs
+++ StateSystem.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.AxHost;
 
 namespace MyFirstWinform
 {
@@ -18,104 +19,59 @@ namespace MyFirstWinform
     {
         private static Image image = Properties.Resources.Incorrect_Guess;
 
-        public string returnCurrentState()
-        {
-            return "No Guess Here";
-        }
-        public Image returnCurrentImage()
-        {
-            return image;
-        }
+        public string returnCurrentState() { return "No Guess Here"; }
+        public Image returnCurrentImage() { return image; }
     }
 
     class IncorrectState : sectionGuessState
     {
         private static Image image = Properties.Resources.Incorrect_Guess;
 
-        public string returnCurrentState()
-        {
-            return "INCORRECT";
-        }
-        public Image returnCurrentImage()
-        {
-            return image;
-        }
+        public string returnCurrentState() { return "INCORRECT"; }
+        public Image returnCurrentImage() { return image; }
     }
 
     class CloseState : sectionGuessState
     {
         private static Image image = Properties.Resources.Close_Guess;
 
-        public string returnCurrentState()
-        {
-            return "CLOSE";
-        }
-        public Image returnCurrentImage()
-        {
-            return image;
-        }
+        public string returnCurrentState() { return "CLOSE"; }
+        public Image returnCurrentImage() { return image; }
     }
 
     class CorrectState : sectionGuessState
     {
         public static Image image = Properties.Resources.Correct_Guess;
 
-        public string returnCurrentState()
-        {
-            return "CORRECT";
-        }
-        public Image returnCurrentImage()
-        {
-            return image;
-        }
+        public string returnCurrentState() { return "CORRECT"; }
+        public Image returnCurrentImage() { return image; }
     }
 
     class WinState : sectionGuessState
     {
         private static Image image = Properties.Resources.Player_Wins;
 
-        public string returnCurrentState()
-        {
-            return "YOU WIN";
-        }
-        public Image returnCurrentImage()
-        {
-            return image;
-        }
+        public string returnCurrentState() { return "YOU WIN"; }
+        public Image returnCurrentImage() { return image; }
     }
 
     class OutOfGuessesState : sectionGuessState
     {
         private static Image image = Properties.Resources.Player_Loses;
 
-        public string returnCurrentState()
-        {
-            return "YOU LOSE";
-        }
-        public Image returnCurrentImage()
-        {
-            return image;
-        }
+        public string returnCurrentState() { return "YOU LOSE"; }
+        public Image returnCurrentImage() { return image; }
     }
 
     class sectionGuessContext
     {
         public sectionGuessState state = new NoState();
 
-        public void setState(sectionGuessState state)
-        {
-            this.state = state;
-        }
+        public void setState(sectionGuessState state) { this.state = state; }
 
-        public string request()
-        {
-            return state.returnCurrentState();
-        }
+        public string request() { return state.returnCurrentState(); }
 
-        public Image getImage()
-        {
-            return state.returnCurrentImage();
-        }
+        public Image getImage() { return state.returnCurrentImage(); }
     }
 
 
@@ -133,7 +89,55 @@ namespace MyFirstWinform
         WinState winState = new WinState();
         OutOfGuessesState outOfGuessesState = new OutOfGuessesState();
 
-        public void SetLastGuessesState(int section, bool correct, bool sectionOver)
+        public StateKeeperImplementation()
+        {
+            lastGuessesState1[0] = new sectionGuessContext();
+            lastGuessesState1[1] = new sectionGuessContext();
+            lastGuessesState2[0] = new sectionGuessContext();
+            lastGuessesState2[1] = new sectionGuessContext();
+            lastGuessesState3[0] = new sectionGuessContext();
+            lastGuessesState3[1] = new sectionGuessContext();
+            ResetKeeper();
+        }
+
+        public void ResetKeeper()
+        {
+            lastGuessesState1[0].setState(noState);
+            lastGuessesState1[1].setState(noState);
+            lastGuessesState2[0].setState(noState);
+            lastGuessesState2[1].setState(noState);
+            lastGuessesState3[0].setState(noState);
+            lastGuessesState3[1].setState(noState);
+        }
+
+        public string GetState(int section, bool first = true)
+        {
+            if (first)
+            {
+                if (section == 1) return lastGuessesState1[0].request();
+                else if (section == 2) return lastGuessesState2[0].request();
+                else if (section == 3) return lastGuessesState3[0].request();
+                else return noState.returnCurrentState();
+            }
+            else
+            {
+                if (section == 1) return lastGuessesState1[1].request();
+                else if (section == 2) return lastGuessesState2[1].request();
+                else if (section == 3) return lastGuessesState3[1].request();
+                else return noState.returnCurrentState();
+            }
+        }
+
+        public Image GetImage(int section)
+        {
+            if (section == 1) return lastGuessesState1[0].getImage();
+            else if (section == 2) return lastGuessesState2[0].getImage();
+            else if (section == 3) return lastGuessesState3[0].getImage();
+            else return noState.returnCurrentImage();
+        }
+
+        // passing anything but 1 2 or 3 will do it to all sections
+        public void SetLastGuessesState(bool correct, bool sectionOver = false, int section = 0, bool close = false)
         {
             if (correct)
             {
@@ -152,49 +156,56 @@ namespace MyFirstWinform
                     lastGuessesState3[1].setState(lastGuessesState3[0].state);
                     lastGuessesState3[0].setState(getCorrectOptions(sectionOver));
                 }
+                else
+                {
+                    lastGuessesState1[1].setState(lastGuessesState1[0].state);
+                    lastGuessesState1[0].setState(getCorrectOptions(sectionOver));
+                    lastGuessesState2[1].setState(lastGuessesState1[0].state);
+                    lastGuessesState2[0].setState(getCorrectOptions(sectionOver));
+                    lastGuessesState3[1].setState(lastGuessesState1[0].state);
+                    lastGuessesState3[0].setState(getCorrectOptions(sectionOver));
+                }
             }
             else
             {
                 if (section == 1)
                 {
                     lastGuessesState1[1].setState(lastGuessesState1[0].state);
-                    lastGuessesState1[0].setState(getIncorrectOptions(sectionOver));
+                    lastGuessesState1[0].setState(getIncorrectOptions(sectionOver, close));
                 }
                 else if (section == 2)
                 {
                     lastGuessesState2[1].setState(lastGuessesState2[0].state);
-                    lastGuessesState2[0].setState(getIncorrectOptions(sectionOver));
+                    lastGuessesState2[0].setState(getIncorrectOptions(sectionOver, close));
                 }
                 else if (section == 3)
                 {
                     lastGuessesState3[1].setState(lastGuessesState3[0].state);
-                    lastGuessesState3[0].setState(getIncorrectOptions(sectionOver));
+                    lastGuessesState3[0].setState(getIncorrectOptions(sectionOver, close));
+                }
+                else
+                {
+                    lastGuessesState1[1].setState(lastGuessesState1[0].state);
+                    lastGuessesState1[0].setState(getIncorrectOptions(sectionOver, close));
+                    lastGuessesState2[1].setState(lastGuessesState1[0].state);
+                    lastGuessesState2[0].setState(getIncorrectOptions(sectionOver, close));
+                    lastGuessesState3[1].setState(lastGuessesState1[0].state);
+                    lastGuessesState3[0].setState(getIncorrectOptions(sectionOver, close));
                 }
             }
         }
 
-        public sectionGuessState getIncorrectOptions(bool sectionOver)
+        public sectionGuessState getIncorrectOptions(bool sectionOver, bool close = false)
         {
-            if (sectionOver)
-            {
-                return outOfGuessesState;
-            }
-            else
-            {
-                return incorrectState;
-            }
+            if (sectionOver) return outOfGuessesState;
+            else if (close) return closeState;
+            else return incorrectState;
         }
 
         public sectionGuessState getCorrectOptions(bool sectionOver)
         {
-            if (sectionOver)
-            {
-                return winState;
-            }
-            else
-            {
-                return correctState;
-            }
+            if (sectionOver) return winState;
+            else return correctState;
         }
 
     }
